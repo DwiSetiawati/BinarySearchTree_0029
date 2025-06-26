f7e3d07d518b28e639a45ca872d1b442172fc9a8
DwiSetiawati <dwisetwa27@gmail.com>
2025-06-26 11:51:06 +0700

    membuat konstruktor untuk class node

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -7,4 +7,11 @@ public:
     int info;
     Node *leftchild;
     Node *rightchild;
+
+    // constructor for the class node
+    Node()
+    {
+        leftchild = nullptr;  // Initialize left chils to null
+        rightchild = nullptr; // Initialize right child to null
+    }
 };
